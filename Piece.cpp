cc661caddf1a1bfdcec6629885b84ad8b9aa5892
Jonathan <joni7840850@gmail.com>
2023-04-03 10:36:41 -0600

    Add comments to methods

--- Piece.cpp
+++ Piece.cpp
@@ -21,6 +21,13 @@ Piece::Piece()
 }
 
 std::string Piece::map_name_to_file_name(){
+    /*
+    This function maps the name of the piece to the file name.
+    Returns:
+        std::string: file name
+    */
+    //Piece name is in the format "name number, for example "pawn 1"
+    //We want to remove the number from the name
     std::string piece_name = m_name.substr(0, m_name.size() - 2);
     if (m_color == "white") {
         return m_images_path + "/w_" + piece_name + ".png";
@@ -31,21 +38,26 @@ std::string Piece::map_name_to_file_name(){
 }
 
 std::pair<int,int> Piece::get_position() const {
+    //Returns the position of the piece
     return m_position;
 }
 
 std::string Piece::get_name() const {
+    //Returns the name of the piece
     return m_name;
 }
 
 std::string Piece::get_color() const {
+    //Returns the color of the piece
     return m_color;
 }
 
 bool Piece::get_is_alive() const {
+    //Returns if the piece is alive or not
     return m_is_alive;
 }
 
 void Piece::set_position(std::pair<int,int> new_position) {
+    //Sets the position of the piece
     m_position = new_position;
 }
