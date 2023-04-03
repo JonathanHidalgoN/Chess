20cf2ec47cacf3dd466406d0cebfb8230ead2378
Jonathan <joni7840850@gmail.com>
2023-04-03 09:20:34 -0600

    Working on adding polymorphism to piece class

--- Piece.cpp
+++ Piece.cpp
@@ -1,69 +1,18 @@
 #include "Piece.h"
 
-Piece::Piece(): x_position {
-    0
-}, y_position {
-    0
-}, m_name {
-    "pawn"
-}, m_color {
-    "white"
-}, m_images_path {
-    "images"
-}, alive {
-    true
-} {}
-
 Piece::Piece(
-        int x = 0,
-        int y = 0,
-        std::string name = "pawn",
-        std::string color = "white",
-        std::string images_path = "images"
-    ): x_position {
-        x
-    }, y_position {
-        y
-    },
-    m_name {
-        name
-    }, m_color {
-        color
-    },
-    m_images_path {
-        images_path
-    }, alive {
-        true
-    } {
-
-    }
-
-void Piece::draw_piece(sf::RenderWindow & window, int height) {
-    sf::Texture pieceTexture;
-    if (!pieceTexture.loadFromFile(map_name())) {
-        //add error handling
-    }
-    if (alive) {
-        // Suppose the height of the window is 800 pixels
-        // Square size is 100 pixels
-        // 
-        int square_size = static_cast < int > (height / 8);
-        int square_number_x = static_cast < int > (x_position / square_size);
-        int square_number_y = static_cast < int > (y_position / square_size);
-        int center_x = square_number_x * square_size - square_size / 8;
-        int center_y = square_number_y * square_size - square_size / 8;
-        int scaling_factor = static_cast < int > (height / 400);
-        sf::Sprite pieceSprite(pieceTexture);
-        pieceSprite.setScale(sf::Vector2f(scaling_factor, scaling_factor));
-        pieceSprite.setPosition(sf::Vector2f(center_x, center_y));
-        window.draw(pieceSprite);
-    } else {
-        //do nothing
-    }
-
+    std::pair<int,int> position,
+    std::string name,
+    std::string color,
+    bool is_alive
+) {
+    m_position = position;
+    m_name = name;
+    m_color = color;
+    m_is_alive = is_alive;
 }
 
-std::string Piece::map_name() {
+std::string Piece::map_name_to_file_name(){
     std::string piece_name = m_name.substr(0, m_name.size() - 2);
     if (m_color == "white") {
         return m_images_path + "/w_" + piece_name + ".png";
@@ -73,25 +22,8 @@ std::string Piece::map_name() {
     return "";
 }
 
-void Piece::follow_mouse(sf::RenderWindow & window) {
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-        x_position = mousePos.x;
-        y_position = mousePos.y;
-    }
-}
-
-void Piece::set_position(int x, int y) {
-    x_position = x;
-    y_position = y;
-}
-
-int Piece::get_x_position() const {
-    return x_position;
-}
-
-int Piece::get_y_position() const {
-    return y_position;
+std::pair<int,int> Piece::get_position() const {
+    return m_position;
 }
 
 std::string Piece::get_name() const {
@@ -100,4 +32,12 @@ std::string Piece::get_name() const {
 
 std::string Piece::get_color() const {
     return m_color;
+}
+
+bool Piece::get_is_alive() const {
+    return m_is_alive;
+}
+
+void Piece::set_position(std::pair<int,int> new_position) {
+    m_position = new_position;
 }
--- Piece.h
+++ Piece.h
@@ -7,41 +7,36 @@
 
 /*
  * Piece class
- * This class is responsible for drawing the pieces on the board.
+ * This class is responsible for creating the pieces.
  * Arguments:
- *   x: x position of the piece  
- *   y: y position of the piece
+ *   position: position of the piece
  *   name: name of the piece
  *   color: color of the piece
- *   images_path: path to the images folder
+ *   is_alive: if the piece is alive or not
  */
 
+
 class Piece {
-    private: int x_position,
-    y_position;
+private:
+    std::pair<int,int> m_position;
     std::string m_name;
     std::string m_color;
+    bool m_is_alive;
     std::string m_images_path;
-    std::string map_name();
+    std::string map_name_to_file_name();
 
-    public: Piece();
-    bool alive;
-
+public:
     Piece(
-        int x,
-        int y,
+        std::pair<int,int> position,
         std::string name,
         std::string color,
-        std::string images_path
+        bool is_alive
     );
-
-    void draw_piece(sf::RenderWindow & window, int height);
-    void follow_mouse(sf::RenderWindow & window);
-    void set_position(int x, int y);
-    int get_x_position() const;
-    int get_y_position() const;
+    std::pair<int,int> get_position() const;
     std::string get_name() const;
     std::string get_color() const;
+    bool get_is_alive() const;
+    void set_position(std::pair<int,int> new_position);
 };
 
 #endif
