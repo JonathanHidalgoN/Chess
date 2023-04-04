e0c5ad7ce6d57a990672adacbf6c52dbfdc2f94d
Jonathan <joni7840850@gmail.com>
2023-04-04 08:22:28 -0600

    Add constructor to chess game

--- main.cpp
+++ main.cpp
@@ -5,38 +5,61 @@
 class ChessGame {
 
 private:
-    sf::RenderWindow m_window;
-    sf::Event m_event;
+    int m_turn{0};
     Board m_board;
- 
+    std::vector<std::pair<int,int>> m_pieces_positions;
+    Piece * m_selected_piece{nullptr};
+    Board m_board;
+    sf::RenderWindow m_window;
 
 public:
-    ChessGame() : 
-    m_board
-        (
-        std::make_pair(800, 800), 
-        std::make_pair(800, 800), 
-        std::make_pair(0, 0), 32
-        )
+    ChessGame():
+        m_board(
+            std::make_pair(400, 400), 
+            std::make_pair(0, 0), 
+            32
+            ),
+        m_window(sf::VideoMode(400, 400), "Chess Game")
     {
-        m_window.create(sf::VideoMode(800, 800), "Chess Game");
-        m_window.setFramerateLimit(60);
-        while (m_window.isOpen()) {
-            while (m_window.pollEvent(m_event)) {
-                if (m_event.type == sf::Event::Closed) {
-                    m_window.close();
-                }
-            }
-            m_board.draw_board(m_window);
-            m_window.clear();
-            m_window.display();
-        }
+        m_window.setFramerateLimit(30);
     }
+
+
+
+
+
 
 
 };
 
+
+
+
+
+
+
+
+
 int main() {
-    ChessGame game;
+    Board board
+        (
+        std::make_pair(400, 400), 
+        std::make_pair(0, 0), 
+        32
+        );
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Chess Game");
+    window.setFramerateLimit(30);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        board.draw_board(window);
+        window.display();
+
+    }
     return 0;
 }
