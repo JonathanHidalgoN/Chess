6e017c2053ec613c7d48b7b70578cf698861e3dd
Jonathan <joni7840850@gmail.com>
2023-04-06 11:47:36 -0600

    Add default argument

--- Model/Moves/MovesStacker.h
+++ Model/Moves/MovesStacker.h
@@ -17,7 +17,8 @@ private:
 public:
     MovesStacker(std::pair<int,int> piece_position);
     std::pair<int,int> stack_move_coords(int x_moves, int y_moves) const;
-    std::vector<std::pair<int,int>> stack_moves_in_direction(int direction, int number_of_moves, std::vector<std::pair<int,int>> possible_moves) const;
+    std::vector<std::pair<int,int>> stack_moves_in_direction(int direction, int number_of_moves, 
+    std::vector<std::pair<int,int>> possible_moves = {}) const;
     std::pair<int,int> get_piece_position() const;
     void change_piece(std::pair<int,int> new_position);
 };
