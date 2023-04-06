13d952b1953c3175c12743d201b50df8bf585da4
Jonathan <joni7840850@gmail.com>
2023-04-06 12:02:14 -0600

    Add method to generate moves for rook piece

--- Model/Moves/MovesValidator.cpp
+++ Model/Moves/MovesValidator.cpp
@@ -25,3 +25,22 @@ std::vector<std::pair<int,int>> MovesValidator::compute_pawn_moves(std::pair<int
     }
     return possible_moves;
 };
+
+std::vector<std::pair<int,int>> MovesValidator::compute_rook_moves(std::pair<int,int> position)
+{
+    /*
+    This function is responsible for generating rook moves.
+    Arguments:
+        position: position of the rook
+    Returns:
+        possible_moves: vector of possible moves
+    */
+    std::vector<std::pair<int,int>> possible_moves = {};
+    m_moves_stacker.change_piece(position);
+
+    possible_moves = (m_moves_stacker.stack_moves_in_direction(1, 7, possible_moves));
+    possible_moves = (m_moves_stacker.stack_moves_in_direction(3, 7, possible_moves));
+    possible_moves = (m_moves_stacker.stack_moves_in_direction(5, 7, possible_moves));
+    possible_moves = (m_moves_stacker.stack_moves_in_direction(7, 7, possible_moves));
+    return possible_moves;
+};
