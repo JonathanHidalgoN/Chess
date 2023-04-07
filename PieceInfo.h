09e1d96698d242943e45ca1b74db26e4a62bdd46
Jonathan <joni7840850@gmail.com>
2023-04-07 17:52:50 -0600

    Add heander commans

--- PieceInfo.h
+++ PieceInfo.h
@@ -1,6 +1,13 @@
+#ifndef PIECEINFO_H
+#define PIECEINFO_H
+
+#include <string>
+#include <utility>
 struct PieceInfo {
     std::pair<int,int> position;
     std::string color;
     std::string name;
     bool alive;
 };
+
+#endif
