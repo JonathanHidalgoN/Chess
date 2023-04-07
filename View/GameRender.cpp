8f06e4bf6455ae8776396b61e5375965ac0c7eab
Jonathan <joni7840850@gmail.com>
2023-04-06 18:12:03 -0600

    Add constructor to ChessRender class

--- View/GameRender.cpp
+++ View/GameRender.cpp
@@ -9,3 +9,14 @@ ChessRender::ChessRender(
 {
     config_window(width, height, title, frame_rate);    
 };
+
+void ChessRender::config_window(
+    const unsigned int width,
+    const unsigned int height,
+    const std::string title,
+    const unsigned int frame_rate
+)
+{
+    m_window.create(sf::VideoMode(width, height), title);
+    m_window.setFramerateLimit(frame_rate);
+}
