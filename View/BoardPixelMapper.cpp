962f76edf7ef31bf7252b395bdb25e1d56103041
Jonathan <joni7840850@gmail.com>
2023-04-06 18:47:32 -0600

    Add methdo to pass from square coords to pixel coords

--- View/BoardPixelMapper.cpp
+++ View/BoardPixelMapper.cpp
@@ -19,3 +19,17 @@ std::pair<unsigned int, unsigned int> BoardPixelMapper::compute_square_dims()
     unsigned int square_height = static_cast<unsigned int>(m_board_size.second / NUMBER_OF_SQUARES);
     return std::pair<unsigned int, unsigned int>(square_width, square_height);
 };
+
+std::pair<unsigned int, unsigned int> BoardPixelMapper::transform_square_coords_to_pixel(std::pair<int, int> square_coords)
+{
+    /*
+    This function transforms the square coordinates to pixel coordinates.
+    Arguments:
+        square_coords: The square coordinates.
+    Returns:
+        The pixel coordinates.
+    */
+    unsigned int x = static_cast<unsigned int>(square_coords.first * m_square_size.first);
+    unsigned int y = static_cast<unsigned int>(square_coords.second * m_square_size.second);
+    return std::pair<unsigned int, unsigned int>(x, y);
+};
