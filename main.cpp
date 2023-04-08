3cbd03cf1858b42ff474fb7930e5126e30415e6a
Jonathan <joni7840850@gmail.com>
2023-04-07 19:48:53 -0600

    Add method to parse pieces info into GameRender

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "Model/ChessModel.h"
 #include "View/GamerRender.h"
+#include "PieceInfo.h"
+#include <iostream>
 
 class ChessGame
 {
@@ -9,14 +11,22 @@ class ChessGame
 private:
     ChessModel m_chess_model;
     ChessRender m_chess_render;
-    void start_game()
+    
+    std::vector<PieceInfo> get_piece_status()
     {
-        /*
-        This function is responsible for starting the game.
-        */
-        m_chess_model.start_game();
-        //function to parse the board and draw the pieces.
+        if(m_chess_model.get_turn() == 0)
+        {
+            m_chess_model.start_game();
+        }
+        std::vector<int> alive_pieces_index = m_chess_model.get_alive_pieces_index();
+        std::vector<PieceInfo> alive_pieces_info;
+        for (int i = 0; i < alive_pieces_index.size(); i++)
+        {
+            alive_pieces_info.push_back(m_chess_model.get_piece_info(alive_pieces_index[i]));
+        }
+        return alive_pieces_info;
     }
+    
 
 public:
     ChessGame(): m_chess_model(), m_chess_render() {};
@@ -29,12 +39,13 @@ public:
         while (m_chess_render.is_open())
         {
             sf::Event event;
-            if (!m_chess_render.render_game(event))
+            if (!m_chess_render.render_game(event, get_piece_status()))
             {
+                std::cout << "Game Over" << std::endl;
                 break;
             }
         }
-    }
+}
 };
 
 int main() {
